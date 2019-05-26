8de2c3b2140b836105d1224074a3e145ba5e61a9
Orres <jinjch@shanghaitech.edu.cn>
2019-05-26 13:28:54 +0800

    finished dataloader, furnish the tensor class

--- include/tensor.hpp
+++ include/tensor.hpp
@@ -7,30 +7,19 @@ using namespace std;
 
 //if it is 2 dimenson then nz is 1;
 class Tensor {
-    int nx;
-    int ny;
-    int nz;
-
-
-
 public:
     Tensor(int x=1, int y=1, int z=1);
-
-    int get_nx(){
-      return nx;
-    }
-    int get_ny(){
-      return ny;
-    }
-    int get_nz(){
-      return nz;
-    }
-
-    //double ***kernel;
-
-    //vector<vector<vector<double> > > kernel (nx, vector<vector<double> >(ny, vector<double>(nz)));
-    vector<vector<vector<double> > > kernel;
+    int get_nx();
+    int get_ny();
+    int get_nz();
+    void set_kernel(vector<vector<vector<double> > >);
     int max_tensor_index(); //return the max element in the tensor
+
+    vector<vector<vector<double> > > _kernel;
+private:
+    int _nx;
+    int _ny;
+    int _nz;
 };
 
 
--- src/tensor.cpp
+++ src/tensor.cpp
@@ -3,5 +3,20 @@
 using namespace std;
 
 
-Tensor::Tensor(int x, int y, int z) :
-        nx(x),ny(y),nz(z) {}
+Tensor::Tensor(int x, int y, int z)
+: _nx(x), _ny(y), _nz(z) {}
+
+int Tensor::get_nx() {
+    return _nx;
+}
+
+int Tensor::get_ny() {
+    return _ny;
+}
+int Tensor::get_nz() {
+    return _nz;
+}
+
+void Tensor::set_kernel(vector<vector<vector<double> > > my_kernel) {
+	_kernel = my_kernel;
+}
