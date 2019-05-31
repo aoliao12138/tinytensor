6b68be52163e84285f76e26a22bd92fdfd7cd8e2
aoliao12138 <czzx_2012_231@126.com>
2019-05-31 16:11:43 +0800

    doxygen latex

--- doc/html/search/all_5.js
+++ doc/html/search/all_5.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['label',['label',['../classMNISTData.html#a087086e85802093365784084d999f859',1,'MNISTData']]],
+  ['laphandle',['LapHandle',['../classswissknife_1_1profiling_1_1LapHandle.html',1,'swissknife::profiling']]],
+  ['laptimer',['LapTimer',['../classswissknife_1_1profiling_1_1LapTimer.html',1,'swissknife::profiling']]],
   ['layer',['Layer',['../classLayer.html',1,'']]],
   ['linear',['Linear',['../classLinear.html',1,'Linear'],['../classLinear.html#a8a079d4750a6f2bb73fe1e4573949058',1,'Linear::Linear()']]],
   ['linearconfigure',['LinearConfigure',['../classLinearConfigure.html',1,'LinearConfigure'],['../classLinearConfigure.html#af020fc317c404ae34427ace4defe9a84',1,'LinearConfigure::LinearConfigure()']]]
--- doc/html/search/classes_1.js
+++ doc/html/search/classes_1.js
@@ -1,5 +1,7 @@
 var searchData=
 [
+  ['laphandle',['LapHandle',['../classswissknife_1_1profiling_1_1LapHandle.html',1,'swissknife::profiling']]],
+  ['laptimer',['LapTimer',['../classswissknife_1_1profiling_1_1LapTimer.html',1,'swissknife::profiling']]],
   ['layer',['Layer',['../classLayer.html',1,'']]],
   ['linear',['Linear',['../classLinear.html',1,'']]],
   ['linearconfigure',['LinearConfigure',['../classLinearConfigure.html',1,'']]]
